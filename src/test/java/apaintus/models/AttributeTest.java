6c4ace3d8996a49f440e270f3db788f3cbb0f4d7
Philippe <philippegarneau1@gmail.com>
2019-02-05 15:37:35 -0500

    Fixed some gradle dependencies for sonar

---
+++ src/test/java/apaintus/models/AttributeTest.java
@@ -0,0 +1,14 @@
+package apaintus.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AttributeTest {
+
+	@Test
+	public void test() {
+	    assertEquals(true, true);
+	}
+
+}
