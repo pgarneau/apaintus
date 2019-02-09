422404422a6aeb340d3cedce2c0d7ae440cfe1e4
Eric <ericbeaudoin1017@gmail.com>
2019-02-09 16:54:04 -0500

    Added application preference save/load
    The data saved isn't correct yet

--- src/main/java/apaintus/Main.java
+++ src/main/java/apaintus/Main.java
@@ -1,5 +1,12 @@
 package apaintus;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 import apaintus.controllers.Controller;
 import apaintus.controllers.MenuController;
 import apaintus.services.PreferencesService;
