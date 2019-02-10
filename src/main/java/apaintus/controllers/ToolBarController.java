e73a9523be46d160b31a7e8a86f2bdbb9088724b
Eric <ericbeaudoin1017@gmail.com>
2019-02-09 20:17:58 -0500

    Added the lastLoad and lastSave repository preferences
    Also added protection against empty text file when first launching

--- src/main/java/apaintus/controllers/ToolBarController.java
+++ src/main/java/apaintus/controllers/ToolBarController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
