7ef16af069a3bd0d7701bc69bb058712708e5ccc
pspino <spip2401@usherbooke.ca>
2019-02-12 22:26:23 -0500

    more test

--- src/test/java/apaintus/models/shapes/TestShapes.java
+++ src/test/java/apaintus/models/shapes/TestShapes.java
@@ -13,26 +13,22 @@ public class TestShapes {
         //Creation attributes...
         ShapeAttributes mockedShapeAttributes = mock(ShapeAttributes.class);
         Point testPoint = new Point(50.0, 50.0);
-        {
-            when(mockedShapeAttributes.getHeight()).thenReturn(2.0);
-            when(mockedShapeAttributes.getWidth()).thenReturn(2.0);
-            when(mockedShapeAttributes.getCoordinates()).thenReturn(testPoint);
-            when(mockedShapeAttributes.getOrientation()).thenReturn(90.0, 0.0);
-            when(mockedShapeAttributes.getStrokeColor()).thenReturn("Black");
-            when(mockedShapeAttributes.getStrokeSize()).thenReturn(6.0);
-        }
+        when(mockedShapeAttributes.getHeight()).thenReturn(2.0);
+        when(mockedShapeAttributes.getWidth()).thenReturn(2.0);
+        when(mockedShapeAttributes.getCoordinates()).thenReturn(testPoint);
+        when(mockedShapeAttributes.getOrientation()).thenReturn(90.0);
+        when(mockedShapeAttributes.getStrokeColor()).thenReturn("Black");
+        when(mockedShapeAttributes.getStrokeSize()).thenReturn(6.0);
 
         //Update attributes...
         ShapeAttributes mockedNewShapeAttributes = mock(ShapeAttributes.class);
         Point testNewPoint = new Point(30.0, 30.0);
-        {
-            when(mockedNewShapeAttributes.getHeight()).thenReturn(6.0);
-            when(mockedNewShapeAttributes.getWidth()).thenReturn(6.0);
-            when(mockedNewShapeAttributes.getCoordinates()).thenReturn(testNewPoint);
-            when(mockedNewShapeAttributes.getOrientation()).thenReturn(0.0, 0.0);
-            when(mockedNewShapeAttributes.getStrokeColor()).thenReturn("White");
-            when(mockedNewShapeAttributes.getStrokeSize()).thenReturn(2.0);
-        }
+        when(mockedNewShapeAttributes.getHeight()).thenReturn(6.0);
+        when(mockedNewShapeAttributes.getWidth()).thenReturn(6.0);
+        when(mockedNewShapeAttributes.getCoordinates()).thenReturn(testNewPoint);
+        when(mockedNewShapeAttributes.getOrientation()).thenReturn(20.0);
+        when(mockedNewShapeAttributes.getStrokeColor()).thenReturn("White");
+        when(mockedNewShapeAttributes.getStrokeSize()).thenReturn(2.0);
 
         //Rectangle
         Rectangle testRect = new Rectangle(mockedShapeAttributes);
@@ -45,7 +41,7 @@ public class TestShapes {
                 () -> assertEquals(2.0, testRect.getHeight(), 0.0),
                 () -> assertEquals(testPoint.getX(), testRect.getCoordinates().getX(), 0.0),
                 () -> assertEquals(testPoint.getY(), testRect.getCoordinates().getY(), 0.0),
-                () -> assertEquals(90.0, testRect.getOrientation(), 0.0),
+                () -> assertEquals("test rect",90.0, testRect.getOrientation(), 0.0),
                 () -> assertEquals("Black", testRect.getStrokeColor()),
                 () -> assertEquals(6.0, testRect.getStrokeSize(), 0.0)
                 ),
@@ -53,7 +49,7 @@ public class TestShapes {
                         () -> assertEquals(2.0, testCircle.getHeight(), 0.0),
                         () -> assertEquals(testPoint.getX(), testCircle.getCoordinates().getX(), 0.0),
                         () -> assertEquals(testPoint.getY(), testCircle.getCoordinates().getY(), 0.0),
-                        () -> assertEquals(90.0, testCircle.getOrientation(), 0.0),
+                        () -> assertEquals("test circle",90.0, testCircle.getOrientation(), 0.0),
                         () -> assertEquals("Black", testCircle.getStrokeColor()),
                         () -> assertEquals(6.0, testCircle.getStrokeSize(), 0.0)
                 ),
@@ -61,7 +57,7 @@ public class TestShapes {
                         () -> assertEquals(2.0, testLine.getHeight(), 0.0),
                         () -> assertEquals(testPoint.getX(), testLine.getCoordinates().getX(), 0.0),
                         () -> assertEquals(testPoint.getY(), testLine.getCoordinates().getY(), 0.0),
-                        () -> assertEquals(90.0, testLine.getOrientation(), 0.0),
+                        () -> assertEquals("test line",90.0, testLine.getOrientation(), 0.0),
                         () -> assertEquals("Black", testLine.getStrokeColor()),
                         () -> assertEquals(6.0, testLine.getStrokeSize(), 0.0)
                 ),
@@ -69,7 +65,7 @@ public class TestShapes {
                         () -> assertEquals(2.0, testSmiley.getHeight(), 0.0),
                         () -> assertEquals(testPoint.getX(), testSmiley.getCoordinates().getX(), 0.0),
                         () -> assertEquals(testPoint.getY(), testSmiley.getCoordinates().getY(), 0.0),
-                        () -> assertEquals(90.0, testSmiley.getOrientation(), 0.0),
+                        () -> assertEquals("test smiley",90.0, testSmiley.getOrientation(), 0.0),
                         () -> assertEquals("Black", testSmiley.getStrokeColor()),
                         () -> assertEquals(6.0, testSmiley.getStrokeSize(), 0.0)
                 ));
@@ -84,7 +80,7 @@ public class TestShapes {
                 () -> assertEquals(6.0, testRect.getHeight(), 0.0),
                 () -> assertEquals(testNewPoint.getX(), testRect.getCoordinates().getX(), 0.0),
                 () -> assertEquals(testNewPoint.getY(), testRect.getCoordinates().getY(), 0.0),
-                () -> assertEquals(0.0, testRect.getOrientation(), 0.0),
+                () -> assertEquals("test rect",20.0, testRect.getOrientation(), 0.0),
                 () -> assertEquals("White", testRect.getStrokeColor()),
                 () -> assertEquals(2.0, testRect.getStrokeSize(), 0.0)
                 ),
@@ -92,7 +88,7 @@ public class TestShapes {
                         () -> assertEquals(6.0, testCircle.getHeight(), 0.0),
                         () -> assertEquals(testNewPoint.getX(), testCircle.getCoordinates().getX(), 0.0),
                         () -> assertEquals(testNewPoint.getY(), testCircle.getCoordinates().getY(), 0.0),
-                        () -> assertEquals(0.0, testCircle.getOrientation(), 0.0),
+                        () -> assertEquals("test circle",20.0, testCircle.getOrientation(), 0.0),
                         () -> assertEquals("White", testCircle.getStrokeColor()),
                         () -> assertEquals(2.0, testCircle.getStrokeSize(), 0.0)
                 ),
@@ -100,7 +96,7 @@ public class TestShapes {
                         () -> assertEquals(6.0, testLine.getHeight(), 0.0),
                         () -> assertEquals(testNewPoint.getX(), testLine.getCoordinates().getX(), 0.0),
                         () -> assertEquals(testNewPoint.getY(), testLine.getCoordinates().getY(), 0.0),
-                        () -> assertEquals(0.0, testLine.getOrientation(), 0.0),
+                        () -> assertEquals("test line",20.0, testLine.getOrientation(), 0.0),
                         () -> assertEquals("White", testLine.getStrokeColor()),
                         () -> assertEquals(2.0, testLine.getStrokeSize(), 0.0)
                 ),
@@ -108,7 +104,7 @@ public class TestShapes {
                         () -> assertEquals(6.0, testSmiley.getHeight(), 0.0),
                         () -> assertEquals(testNewPoint.getX(), testSmiley.getCoordinates().getX(), 0.0),
                         () -> assertEquals(testNewPoint.getY(), testSmiley.getCoordinates().getY(), 0.0),
-                        () -> assertEquals(0.0, testSmiley.getOrientation(), 0.0),
+                        () -> assertEquals("test smiley",20.0, testSmiley.getOrientation(), 0.0),
                         () -> assertEquals("White", testSmiley.getStrokeColor()),
                         () -> assertEquals(2.0, testSmiley.getStrokeSize(), 0.0)
                 ));
