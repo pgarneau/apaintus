5371558c8eb85031a012dbf8e7e5ef34f0ee4c3e
Philippe Spino <philippe.spino@usherbrooke.ca>
2019-02-17 10:01:38 -0500

    added draw image command test

---
+++ src/test/java/apaintus/models/commands/TestDrawImageCommand.java
@@ -0,0 +1,64 @@
+package apaintus.models.commands;
+
+import apaintus.controllers.CanvasController;
+import javafx.scene.image.Image;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestDrawImageCommand {
+    @Test
+    public void testCreateCommand(){
+        CanvasController testCanvasController = mock(CanvasController.class);
+        Image testImage = mock(Image.class);
+        DrawImageCommand test = new DrawImageCommand(testCanvasController,testImage);
+        assertNotNull(test);
+    }
+
+    @Test
+    void testExecute() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        Image testImage = mock(Image.class);
+
+        doCallRealMethod().when(testCanvasController).getBaseImage();
+        doCallRealMethod().when(testCanvasController).setBaseImage(nullable(Image.class));
+        DrawImageCommand test = new DrawImageCommand(testCanvasController,testImage);
+
+        test.execute();
+
+        assertNotNull(testCanvasController.getBaseImage());
+    }
+
+    @Test
+    void testUndo() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        Image testImage = mock(Image.class);
+
+        doCallRealMethod().when(testCanvasController).getBaseImage();
+        doCallRealMethod().when(testCanvasController).setBaseImage(nullable(Image.class));
+        DrawImageCommand test = new DrawImageCommand(testCanvasController,testImage);
+
+        test.execute();
+        test.undo();
+
+        assertNull(testCanvasController.getBaseImage());
+    }
+
+    @Test
+    void testRedo() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        Image testImage = mock(Image.class);
+
+        doCallRealMethod().when(testCanvasController).getBaseImage();
+        doCallRealMethod().when(testCanvasController).setBaseImage(nullable(Image.class));
+        DrawImageCommand test = new DrawImageCommand(testCanvasController,testImage);
+
+        test.execute();
+        test.undo();
+        test.redo();
+
+        assertEquals(testCanvasController.getBaseImage(),testImage);
+
+    }
+}
