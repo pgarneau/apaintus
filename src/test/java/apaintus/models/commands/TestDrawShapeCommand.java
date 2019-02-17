5bc347c9492005c0a0e883b7b8c425649dd1381d
Philippe Spino <philippe.spino@usherbrooke.ca>
2019-02-17 10:16:38 -0500

    added draw shape command test without assertion

---
+++ src/test/java/apaintus/models/commands/TestDrawShapeCommand.java
@@ -0,0 +1,83 @@
+package apaintus.models.commands;
+
+import apaintus.controllers.CanvasController;
+import apaintus.models.shapes.DrawableShape;
+import apaintus.models.shapes.ShapeType;
+import javafx.scene.image.Image;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TestDrawShapeCommand {
+    @Test
+    public void testCreateDrawShapeCommand() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        DrawableShape testShape = mock(DrawableShape.class);
+        SelectCommand testSelectCommand = mock(SelectCommand.class);
+
+        DrawShapeCommand test = new DrawShapeCommand(testCanvasController, testShape, testSelectCommand);
+
+        assertNotNull(test);
+    }
+
+    @Test
+    void testExecute() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        DrawableShape testShape = mock(DrawableShape.class);
+        SelectCommand testSelectCommand = mock(SelectCommand.class);
+
+        doCallRealMethod().when(testCanvasController).saveDrawLayer();
+
+        when(testShape.getShapeType()).thenReturn(ShapeType.RECTANGLE);
+
+        doAnswer((i) -> {
+            return null;
+        }).when(testSelectCommand).execute();
+
+        DrawShapeCommand test = new DrawShapeCommand(testCanvasController, testShape, testSelectCommand);
+        test.execute();
+
+        //Need to check later what assertion we do want to do.
+        //assertFalse(testCanvasController.getDrawnShapes().isEmpty());
+    }
+
+    @Test
+    void testUndo() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        DrawableShape testShape = mock(DrawableShape.class);
+        SelectCommand testSelectCommand = mock(SelectCommand.class);
+
+        doCallRealMethod().when(testCanvasController).saveDrawLayer();
+
+        when(testShape.getShapeType()).thenReturn(ShapeType.RECTANGLE);
+
+        doAnswer((i) -> {
+            return null;
+        }).when(testSelectCommand).execute();
+
+        DrawShapeCommand test = new DrawShapeCommand(testCanvasController, testShape, testSelectCommand);
+        test.execute();
+        test.undo();
+    }
+
+    @Test
+    void testRedo() {
+        CanvasController testCanvasController = mock(CanvasController.class);
+        DrawableShape testShape = mock(DrawableShape.class);
+        SelectCommand testSelectCommand = mock(SelectCommand.class);
+
+        doCallRealMethod().when(testCanvasController).saveDrawLayer();
+
+        when(testShape.getShapeType()).thenReturn(ShapeType.RECTANGLE);
+
+        doAnswer((i) -> {
+            return null;
+        }).when(testSelectCommand).execute();
+
+        DrawShapeCommand test = new DrawShapeCommand(testCanvasController, testShape, testSelectCommand);
+        test.execute();
+        test.undo();
+        test.redo();
+    }
+}
