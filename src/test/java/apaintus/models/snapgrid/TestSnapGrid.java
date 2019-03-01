7ce9737bb652dcd66185ba06f5101bdb43e8dc05
Philippe <philippegarneau1@gmail.com>
2019-03-01 10:14:45 -0500

    Added more tests

--- src/test/java/apaintus/models/snapgrid/TestSnapGrid.java
+++ src/test/java/apaintus/models/snapgrid/TestSnapGrid.java
@@ -2,71 +2,42 @@ package apaintus.models.snapgrid;
 
 import apaintus.models.Point;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestSnapGrid {
-//    double testSpacing = 4.0;
-//    double testCanvasWidth = 600.0;
-//    double testCanvasHeight = 800.0;
-//    boolean active = false;
-//
-//    @test
-//    public void testsnapgridallpoints() {
-//
-//        testspacing = 0.0;
-//        snapgrid test = null;
-//        try {
-//            test = new snapgrid(testspacing,
-//                    testcanvaswidth,
-//                    testcanvasheight,
-//                    active);
-//        } catch (exception e) {
-//            assertnull(test);
-//        }
-//
-//        testspacing = 4.0;
-//        test = new snapgrid(testspacing,
-//                testcanvaswidth,
-//                testcanvasheight,
-//                active);
-//        arraylist<point> goodpoints = new arraylist();
-//        for (int y = 0; y <= testcanvasheight; y += testspacing * 10) {
-//            for (int x = 0; x <= testcanvaswidth; x += testspacing * 10) {
-//                goodpoints.add(new point(x, y));
-//            }
-//        }
-//
-//        for (int i = 0; i < goodpoints.size(); i++) {
-//            assertequals(goodpoints.get(i).getx(), test.getgridpoints().get(i).getx(),0.1);
-//            assertequals(goodpoints.get(i).getx(), test.getgridpoints().get(i).getx(),0.1);
-//        }
-//    }
-//
-//    @test
-//    public void testactivatedeactivatesnapgrid(){
-//
-//        snapgrid test = new snapgrid(testspacing,
-//                testcanvaswidth,
-//                testcanvasheight,
-//                active);
-//
-//        test.toggle(true);
-//        asserttrue(test.isactive());
-//
-//        test.toggle(false);
-//        assertfalse(test.isactive());
-//    }
-//
-//    @test
-//    public void testspacing(){
-//        snapgrid test = new snapgrid(testspacing,
-//                testcanvaswidth,
-//                testcanvasheight,
-//                active);
-//
-//        test.setgradation(2.0);
-//        assertequals(20.0,test.getgradation(),0.0);
-//    }
+    @Test
+    public void setGradationThenSuccessful() {
+        // Given
+        double width = 0;
+        double height = 0;
+        double size = 1;
+        int expectedSnapGridPointsSize = 1;
+        SnapGrid snapGrid = new SnapGrid(width, height);
+
+        // When
+        snapGrid.setGradation(size);
+        List<Point> snapGridPoints = snapGrid.getSnapGridPoints();
+
+        // Then
+        assertEquals(expectedSnapGridPointsSize, snapGridPoints.size());
+        assertEquals(size, snapGrid.getGradation(), 0.1);
+    }
+
+    @Test
+    public void toggleThenSuccessful() {
+        // Given
+        double width = 0;
+        double height = 0;
+        SnapGrid snapGrid = new SnapGrid(width, height);
+
+        // When
+        snapGrid.toggle();
+
+        // Then
+        assertTrue(snapGrid.isActive());
+    }
 }
